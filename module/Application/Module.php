583bb7a20cbe2c39a19ee0a28c81f550e0039a48
Benjamin Tigano <bentigano@gmail.com>
2013-12-19 18:18:49 -0500

    Removed test comment

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -6,8 +6,6 @@
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
- 
- TEST
 
 namespace Application;
 
