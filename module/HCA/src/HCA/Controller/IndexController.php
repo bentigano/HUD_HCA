f1c45905840bbc4b37345ad7bbca37d58ac39a9d
Benjamin Tigano <bentigano@gmail.com>
2014-02-25 15:14:13 -0500

    Added system checks
    Added a controller action that allows you to run checks for database
    connectivity, required tables, and required data.

--- module/HCA/src/HCA/Controller/IndexController.php
+++ module/HCA/src/HCA/Controller/IndexController.php
@@ -8,6 +8,7 @@ use Zend\View\Model\ViewModel;
 class IndexController extends AbstractActionController
 {
     protected $agencyTable;
+	protected $zipCodeTable;
     
     public function indexAction()
     {
@@ -18,6 +19,47 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+	
+	public function checkSystemAction()
+    {
+		$results = array();
+		
+		// check database connection
+		try {
+			$this->getAgencyTable()->fetchAll()->count();
+			$results['DB'] = array('result' => true, 'message' => 'Successfully connected to database!');
+		} catch (\Exception $e) {
+			$results['DB'] = array('result' => false, 'message' => 'Unable to access the database. Please check your configuration.');
+		}
+		
+		// ensure all required tables exist
+		try {
+			$count = $this->getAgencyTable()->fetchAll()->count();
+			$results['TBL_AGENCIES'] = array('result' => true, 'message' => 'Successfully queried agencies table in database!');
+			if ($count > 0) {
+				$results['TBL_AGENCIES_COUNT'] = array('result' => true, 'message' => 'Data successfully found in agencies table!');
+			} else {
+				$results['TBL_AGENCIES_COUNT'] = array('result' => false, 'message' => 'There is no data in the agencies table. Please import agency data from HUD.');
+			}
+		} catch (\Exception $e) {
+			$results['TBL_AGENCIES'] = array('result' => false, 'message' => 'Unable to access the agencies table. Please check your database schema.');
+		}
+		
+		try {
+			$count = $this->getZipCodeTable()->fetchAll()->count();
+			$results['TBL_ZIPS'] = array('result' => true, 'message' => 'Successfully queried zip_codes table in database!');
+			if ($count > 0) {
+				$results['TBL_ZIPS_COUNT'] = array('result' => true, 'message' => 'Data successfully found in zip_codes table!');
+			} else {
+				$results['TBL_ZIPS_COUNT'] = array('result' => false, 'message' => 'There is no data in the zip_codes table. Please import all zip code data.');
+			}
+		} catch (\Exception $e) {
+			$results['TBL_ZIPS'] = array('result' => false, 'message' => 'Unable to access the zip_codes table. Please check your database schema.');
+		}
+		
+        $view = new ViewModel($results);
+        return $view;
+    }
     
     public function getAgencyTable()
     {
@@ -26,5 +68,14 @@ class IndexController extends AbstractActionController
             $this->agencyTable = $sm->get('HCA\Model\AgencyTable');
         }
         return $this->agencyTable;
+    }
+	
+	private function getZipCodeTable()
+    {
+        if (!$this->zipCodeTable) {
+            $sm = $this->getServiceLocator();
+            $this->zipCodeTable = $sm->get('HCA\Model\ZipCodeTable');
+        }
+        return $this->zipCodeTable;
     }
 }
